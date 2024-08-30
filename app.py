b2ee5f31072485a9cd03ea4e2a28b5de0eed4423
Abhilash Maurya <88419592+SnoopyAbhilash@users.noreply.github.com>
2024-08-30 22:21:48 +0530

    Update app.py
    some comment are updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-# DAY NAME  OF THE GIVEN DATE CALCULATOR ALSO TELLS TAHT GIVEN YEAR IS LEAP YEAR OR NOT 
+# DAY NAME  OF THE GIVEN DATE CALCULATOR ALSO TELLS THAT THE GIVEN YEAR IS LEAP YEAR OR NOT 
 
 
 def day_of_week_calculator(day, month, year):
@@ -19,14 +19,15 @@ def day_of_week_calculator(day, month, year):
     return days[day_of_the_week]
 
 
-# Function to check provided year is leap year or not
+# Function to check given year is leap year or not
+
 def isleap_year(year):
     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
         return True
     return False
 
 
-# validating the provied date 
+# validating the given date 
 
 def validate_date(day, month, year):
     if not (1 <= month <= 12):
@@ -59,14 +60,14 @@ try:
     
     else:
 
-        # Check if the year is a leap year
+        # message for leap year
         leap_year_message = "This year is a leap year." if isleap_year(year) else "This year is not a leap year."
 
         # Calculate the day of the week
         day_of_week = day_of_week_calculator(day, month, year)
         
 
-        # showing final output
+        # showing final result
 
         print(f"The day of the week for {date} is {day_of_week}.")
         print(leap_year_message)
